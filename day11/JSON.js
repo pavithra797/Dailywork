1bacea0f6b2e0b68aa04765a8b48dcac4334aa18
pavithra <pavithraktek@gmail.com>
2026-03-26 12:32:03 +0530

    to-do app

---
+++ day11/JSON.js
@@ -0,0 +1,8 @@
+const JSON = [
+    { id: 1, name: "A", active: true },
+    { id: 2, name: "B", active: false },
+    { id: 3, name: "C", active: true },
+];
+
+const users = JSON.parse(json);
+console.log(users[0]);
---
+++ day11/objects.js
@@ -0,0 +1,15 @@
+const users= [
+    { id: 1, name: "A", active: true },
+    { id: 2, name: "B", active: false },
+    { id: 3, name: "C", active: true },
+];
+
+// function countActive(usersArray) {
+//     const activeUsers = usersArray.filter(user => user.active);
+//     console.log("Active users:", activeUsers);
+//     return activeUsers.length;
+// }
+
+// const activeCount = countActive(users);
+// console.log(activeCount);
+
