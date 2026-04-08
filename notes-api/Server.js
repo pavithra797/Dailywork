c8ed622d30523e340cb933e2d5f9c7cbeb99321c
pavithra <pavithraktek@gmail.com>
2026-04-08 21:06:08 +0530

    day17

--- notes-api/Server.js
+++ notes-api/Server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 const notesRoutes = require('./routes/notesRoutes');
+console.log('error', notesRoutes);
+const corsOptions = {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"]
+};
 
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/notes', notesRoutes);
-
-app.listen(3000, () => {
-    console.log('Server started');
+app.listen(3001, ()=>{
+    console.log("Server Started at port 3001");
 });
 
 module.exports = app;
